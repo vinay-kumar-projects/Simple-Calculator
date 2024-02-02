3211d53378899a6e7b8fc14d35020ee9500fcf88
vinay-kumar-projects <vinayprojects@gmail.com>
2024-02-02 14:34:39 +0530

    Making a simple calculator using ReactJS

---
+++ src/Body.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {add, sub, mult, div} from './Calc'
+
+function Body() {
+
+return (
+    <>
+    <ul>
+        <li>Add of two numbers is: {add(11,32)}</li>
+        <li>Sub of two numbers is: {sub(65,32)}</li>
+        <li>Multi of two numbers is: {mult(15,53)}</li>
+        <li>Div of two numbers is: {div(20,3)}</li>
+    </ul>
+</>
+);
+}
+export default Body
